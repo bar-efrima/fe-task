69bc74c7228c7a7badb853ac64e1c0fdb2d5f080
Bar Efrima <bar.efrima@post.runi.ac.il>
2024-06-26 17:48:22 +0300

    Get Pokemons function, with test

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 import "./App.css";
+import { getPokemons } from './services/pokemon.service';
+import { useEffect, useState } from 'react';
 
 function App() {
+  // test
+  useEffect(() => {
+    // Test getPokemons function
+    const fetchPokemons = async () => {
+      const pokemons = await getPokemons();
+      console.log('Pokémon List:', pokemons);
+    };
+    fetchPokemons();
+  }, []);
   return (
     <>
     <h1>Pokedex with Health.io</h1>
--- src/services/pokemon.service.js
+++ src/services/pokemon.service.js
@@ -1,5 +1,19 @@
-export async function getPokemons() {
+import axios from 'axios';
+
+// Base URL for the Pokémon API
+const API_URL = 'https://pokeapi.co/api/v2/pokemon';
+
+export async function getPokemons(limit = 150) {
   // Fetch the list of pokemons from the API
+  try {
+    const response = await axios.get(`${API_URL}?limit=${limit}`);
+    // return the list of Pokémon
+    return response.data.results;
+}catch (error) {
+  console.error('Error fetching Pokémon list:', error);
+  // Return empty array if error
+  return [];
+}
 }
 
 export async function getPokemonDetailsByURL(url) {
