af21f6601cd2c7195d835bf324d5de47189ed6f3
Bar Efrima <bar.efrima@post.runi.ac.il>
2024-06-27 10:44:05 +0300

    favorites.service with tests

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import "./App.css";
 
 import { useEffect, useState } from 'react';
 import { getPokemons, getPokemonDetailsByURL } from './services/pokemon.service';
+import { getFavorites, addFavorite, isFavorite } from './services/favorites.service';
 
 function App() {
   // tests 
   useEffect(() => {
-    // Test getPokemons function
+    // Test getPokemons 
     const fetchPokemons = async () => {
       const pokemons = await getPokemons();
       console.log('Pokémon List:', pokemons);
@@ -16,6 +17,15 @@ function App() {
         const Pokemon1URL = pokemons[0].url;
         const pokemonDetails = await getPokemonDetailsByURL(Pokemon1URL);
         console.log('First Pokémon Details:', pokemonDetails);
+
+      await addFavorite(pokemonDetails);
+      console.log('Added to Favorites:', pokemonDetails);
+
+      const favorites = await getFavorites();
+      console.log('Favorites List:', favorites);
+
+      const isFav = isFavorite(pokemonDetails);
+      console.log(`Is ${pokemonDetails.name} a favorite?`, isFav);
       }
     };
     fetchPokemons();
--- src/services/favorites.service.js
+++ src/services/favorites.service.js
@@ -11,9 +11,18 @@ export async function getFavorites() {
 
 export async function addFavorite(pokemon) {
   // Add the pokemon to the favorites list
+  // Get the favorites list from local storage of the browser
+  return new Promise((resolve) => {
+    const favorites = JSON.parse(localStorage.getItem("favorites")) || []; 
+    favorites.push(pokemon);
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+    setTimeout(() => {
+      resolve(favorites);
+    }, 500);
+  });
 }
 
-
 export function isFavorite(pokemon) {
-  // Check if the pokemon is in the favorites list
+  const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+  return favorites.some(fav => fav.id === pokemon.id);
 }
